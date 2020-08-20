15ff428ea791567b6e94de8ea47e9e4296d6e8da
mastergoster <dugrais.julien@icloud.com>
2020-08-20 05:00:53 +0200

    update

--- src/Controller/Admin/AdminCoreController.php
+++ src/Controller/Admin/AdminCoreController.php
@@ -16,9 +16,9 @@ class AdminCoreController extends Controller
 
     public function update()
     {
-        // if (\getenv("ENV_DEV")) {
-        //     return dd("mode dev impossible de faire ceci");
-        // }
+        if (\getenv("ENV_DEV")) {
+            return dd("mode dev impossible de faire ceci");
+        }
         exec("cd .. && git pull", $returns);
         exec("cd .. && composer update", $returns2);
         dump($returns);
