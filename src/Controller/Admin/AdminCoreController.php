5ef4eecdbbab2c5a59cc47d7d0a512c1c85af0e8
mastergoster <dugrais.julien@icloud.com>
2020-08-22 00:13:03 +0200

    events

--- src/Controller/Admin/AdminCoreController.php
+++ src/Controller/Admin/AdminCoreController.php
@@ -21,23 +21,6 @@ class AdminCoreController extends Controller
             return dd("mode dev impossible de faire ceci");
         }
         chdir("/var/www");
-        putenv("COMPOSER_HOME=/var/www/.config/composer");
-
-        if (!file_exists("composer.phar")) {
-
-
-            $source = "https://getcomposer.org/installer";
-            $destination = "/var/www/composer-setup.php";
-
-            $data = file_get_contents($source);
-            $file = fopen($destination, "w+");
-            fputs($file, $data);
-            fclose($file);
-
-            echo `php composer-setup.php`;
-            echo `php -r "unlink('composer-setup.php');"`;
-        }
-
         exec("sudo git pull", $git);
         $composer = `composer update -v --no-dev -o 2>&1`;
         $composer2 = [];
